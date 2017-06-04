611f7faa01c6af8e0871360262fa07bb8b14d541
Dylan Smith <dylan.smith888@gmail.com>
2017-06-04 21:16:53 +0200

    Playing around the learning rate and making a prediction

--- w1/ex1/ex1_multi.m
+++ w1/ex1/ex1_multi.m
@@ -95,6 +95,39 @@ plot(1:numel(J_history), J_history, '-b', 'LineWidth', 2);
 xlabel('Number of iterations');
 ylabel('Cost J');
 
+hold on;
+
+% test 1
+alpha = 0.3; % descends far too quickly that is overshoots the minimum
+num_iters = 400;
+theta = zeros(3, 1);
+[theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters);
+plot(1:numel(J_history), J_history, '-r', 'LineWidth', 2);
+
+% test 2
+alpha = 0.03;
+num_iters = 400;
+theta = zeros(3, 1);
+[theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters);
+plot(1:numel(J_history), J_history, '-g', 'LineWidth', 2);
+
+% test 3
+alpha = 0.1;
+num_iters = 400;
+theta = zeros(3, 1);
+[theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters);
+plot(1:numel(J_history), J_history, '-y', 'LineWidth', 2);
+
+% test 3 -- descends far to slowly (so number of  iterations must increase to anything useful on the plot)
+alpha = 0.0001;
+num_iters = 40000; 
+theta = zeros(3, 1);
+[theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters);
+plot(1:numel(J_history), J_history, '-m', 'LineWidth', 2);
+
+hold off;
+
+
 % Display gradient descent's result
 fprintf('Theta computed from gradient descent: \n');
 fprintf(' %f \n', theta);
@@ -104,7 +137,7 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
+price = [1, 1.650, 3] * theta; 
 
 
 % ============================================================
@@ -149,8 +182,7 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
-
+price = [1, 1650, 3] * theta; 
 
 % ============================================================
 
