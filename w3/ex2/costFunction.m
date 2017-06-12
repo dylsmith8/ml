3e6f1579095f42f6204c6e20a93c03c696b199ed
Dylan Smith <dylan.smith888@gmail.com>
2017-06-12 20:36:39 +0200

    Logistic Regression costFunction and gradient computation

--- w3/ex2/costFunction.m
+++ w3/ex2/costFunction.m
@@ -4,12 +4,12 @@ function [J, grad] = costFunction(theta, X, y)
 %   parameter for logistic regression and the gradient of the cost
 %   w.r.t. to the parameters.
 
-% Initialize some useful values
-m = length(y); % number of training examples
+	% Initialize some useful values
+	m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
+	% You need to return the following variables correctly 
+	J = 0;
+	grad = zeros(size(theta));
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
@@ -19,14 +19,12 @@ grad = zeros(size(theta));
 %
 % Note: grad should have the same dimensions as theta
 %
+	firstTerm = -y' * (log(sigmoid(X * theta)));
+	secondTerm = -1 .* ((1 - y') * log(1 - (sigmoid(X  * theta))));
+	J = (1 / m) * sum(firstTerm + secondTerm);
 
-
-
-
-
-
-
-
+	%grad  = (1 / m) * sum(((X * theta) - y) .* X);
+	grad = (1 / m) * X' * ((sigmoid(X * theta)) - y);
 % =============================================================
 
 end
