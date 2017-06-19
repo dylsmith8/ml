f8304dc984bb45fd973a50ad0efb2f5c2532f3a3
Dylan Smith <dylan.smith888@gmail.com>
2017-06-19 14:00:37 +0200

    nn predict (WIP)

--- w4/ex3/predict.m
+++ w4/ex3/predict.m
@@ -3,12 +3,12 @@ function p = predict(Theta1, Theta2, X)
 %   p = PREDICT(Theta1, Theta2, X) outputs the predicted label of X given the
 %   trained weights of a neural network (Theta1, Theta2)
 
-% Useful values
-m = size(X, 1);
-num_labels = size(Theta2, 1);
+	% Useful values
+	m = size(X, 1);
+	num_labels = size(Theta2, 1);
 
-% You need to return the following variables correctly 
-p = zeros(size(X, 1), 1);
+	% You need to return the following variables correctly 
+	p = zeros(size(X, 1), 1);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
@@ -20,16 +20,10 @@ p = zeros(size(X, 1), 1);
 %       information see 'help max'. If your examples are in rows, then, you
 %       can use max(A, [], 2) to obtain the max for each row.
 %
+	[test1, test2] = max(X, [], 2);
 
-
-
-
-
-
-
-
-
+	p = (test2' * Theta1) + (test2' * Theta2);
+	%[maximumValue, indices] = max ( + (Theta2 * X'));
+	%p = indices';
 % =========================================================================
-
-
 end
