7e94f5e541e7848eacff2902f87bad7db808b8a9
Dylan Smith <dylan.smith888@gmail.com>
2017-06-19 12:42:31 +0200

    one vs all

--- w4/ex3/oneVsAll.m
+++ w4/ex3/oneVsAll.m
@@ -7,15 +7,15 @@ function [all_theta] = oneVsAll(X, y, num_labels, lambda)
 %   in a matrix all_theta, where the i-th row of all_theta corresponds 
 %   to the classifier for label i
 
-% Some useful variables
-m = size(X, 1);
-n = size(X, 2);
+	% Some useful variables
+	m = size(X, 1);
+	n = size(X, 2);
 
-% You need to return the following variables correctly 
-all_theta = zeros(num_labels, n + 1);
+	% You need to return the following variables correctly 
+	all_theta = zeros(num_labels, n + 1);
 
-% Add ones to the X data matrix
-X = [ones(m, 1) X];
+	% Add ones to the X data matrix
+	X = [ones(m, 1) X];
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: You should complete the following code to train num_labels
@@ -48,19 +48,11 @@ X = [ones(m, 1) X];
 %         fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), ...
 %                 initial_theta, options);
 %
+	initial_theta = zeros(n + 1, 1);
+	options = optimset('GradObj', 'on', 'MaxIter', 50);
 
-
-
-
-
-
-
-
-
-
-
-
+	for i = 1 : num_labels,
+		all_theta(i, :) =  fmincg (@(t)(lrCostFunction(t, X, (y == i), lambda)), initial_theta, options);
+	end
 % =========================================================================
-
-
 end
