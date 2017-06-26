06c931ae36e22428523131b2bbd60f22ee9f16c8
Dylan Smith <dylan.smith888@gmail.com>
2017-06-26 10:57:27 +0200

    LG cost function

--- w6/ex5/linearRegCostFunction.m
+++ w6/ex5/linearRegCostFunction.m
@@ -6,11 +6,11 @@ function [J, grad] = linearRegCostFunction(X, y, theta, lambda)
 %   data points in X and y. Returns the cost in J and the gradient in grad
 
 % Initialize some useful values
-m = length(y); % number of training examples
+	m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
+	% You need to return the following variables correctly 
+	J = 0;
+	grad = zeros(size(theta));
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost and gradient of regularized linear 
@@ -19,19 +19,21 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
+	% cost 
+	predictions = X * theta; 
+	sqrErrors = (predictions - y).^2;
+	costUnregularised = 1 / (2 * m) * sum(sqrErrors);
 
+	regTerm = (lambda / (2 * m)) * sum([0; theta(2 : end, :)] .^ 2);
 
+	J = costUnregularised+ regTerm;
 
-
-
-
-
-
-
+	% grad
+	
 
 
 % =========================================================================
 
-grad = grad(:);
+	grad = grad(:);
 
 end
